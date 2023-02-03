e075d046d47c4a0bab2c239869a6bc3ee0302256
Max <40802716+mcalle1@users.noreply.github.com>
2023-02-03 02:00:56 +0000

    part 4 brute force solution

--- Python/4.py
+++ Python/4.py
@@ -5,13 +5,12 @@ A palindromic number reads the same both ways. The largest palindrome made
 Find the largest palindrome made from the product of two 3-digit numbers.
 '''
 
-# how do you create a palindromic number?
-three_digits = [i for i in range(100, 1000)]
-smallest_mul = three_digits[0]**2
-largest_mul = three_digits[-1]**2
-# maximum must be less than 888888
-# and greater than 10001
-dig = 6
-def find_pal(digits):
-    ...
-print(smallest_mul, largest_mul)
+def pal(low, high):
+    for i in range(high, low, -1):
+        for j in range(high, low, -1):
+            candidate = str(i * j)
+            if candidate == candidate[::-1]:
+                print(i, j)
+                yield int(candidate)
+
+print(max(list(pal(100,999))))
